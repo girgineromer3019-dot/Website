aee13615e18681d18d229897494c064c0adecefc
OMER <girgineromer3019@gmail.com>
2025-10-02 14:24:19 +0200

    WebSite
    Basis of MijnWebsite

--- OmerGIRGINER.js
+++ OmerGIRGINER.js
@@ -1,3 +1,11 @@
-function showEmail() {
-  document.getElementById("email").textContent = "ornekmail@domain.com";
-}
+document.getElementById("mailButton").addEventListener("click", function() {
+  window.location.href = "mailto:girgineromer3019@gmail.com";
+});
+
+document.getElementById("GitHubButton").addEventListener("click", function() {
+  window.location.href = "https://github.com/girgineromer3019-dot";
+});
+
+document.getElementById("whatsappButton").addEventListener("click", function() {
+    window.location.href = "https://wa.me/31628617543?text=Hallo!";
+});
