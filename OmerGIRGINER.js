e6d01998001520a584c6a3b9ac05bd3bc413aa81
OMER <girgineromer3019@gmail.com>
2025-10-03 11:00:21 +0200

    TweedeDagWebsiteBasis
    Deze dag is tweede dag voor basis website over mij

--- OmerGIRGINER.js
+++ OmerGIRGINER.js
@@ -1,11 +1,11 @@
 document.getElementById("mailButton").addEventListener("click", function() {
-  window.location.href = "mailto:girgineromer3019@gmail.com";
+  window.open("mailto:girgineromer3019@gmail.com", "_blank");
 });
 
 document.getElementById("GitHubButton").addEventListener("click", function() {
-  window.location.href = "https://github.com/girgineromer3019-dot";
+  window.open("https://github.com/girgineromer3019-dot", "_blank");
 });
 
 document.getElementById("whatsappButton").addEventListener("click", function() {
-    window.location.href = "https://wa.me/31628617543?text=Hallo!";
+  window.open("https://wa.me/31628617543?text=Hallo!", "_blank");
 });
