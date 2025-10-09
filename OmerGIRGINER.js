2dad72d80c1b01f8fad28586086c21509581496f
OMER <girgineromer3019@gmail.com>
2025-10-09 12:22:43 +0200

    Website
    Ik heb een paar update gemaak. (Mauseefeckt en Header :hover)

--- OmerGIRGINER.js
+++ OmerGIRGINER.js
@@ -6,6 +6,22 @@ document.getElementById("GitHubButton").addEventListener("click", function() {
   window.open("https://github.com/girgineromer3019-dot", "_blank");
 });
 
+document.getElementById("LinkedButton").addEventListener("click", function(){
+  window.open("https://www.linkedin.com/feed/", "_blank");
+})
+
 document.getElementById("whatsappButton").addEventListener("click", function() {
   window.open("https://wa.me/31628617543?text=Hallo!", "_blank");
+});
+
+
+document.addEventListener("mousemove", (e) => { // Mausun haraketini senkronize eder
+  const light = document.createElement("div");
+  light.classList.add("light");
+  light.style.left = e.clientX + "px";
+  light.style.top = e.clientY + "px";
+  document.body.appendChild(light);
+
+  // Animasyon tamamlanınca DOM'dan kaldır
+  setTimeout(() => light.remove(), 800);
 });
